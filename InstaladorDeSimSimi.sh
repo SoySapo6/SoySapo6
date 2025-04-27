cb56ad81287bb09f3bb9958bab3f4aa3b55504a4
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-04-27 13:27:46 -0500

    SoyMaycol

---
+++ InstaladorDeSimSimi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+tempfile="/tmp/simichat.js"
+curl -s https://raw.githubusercontent.com/SoySapo6/SoySapo6/refs/heads/main/NightAPI-Simi.js > "$tempfile"
+node "$tempfile"
+rm "$tempfile"
